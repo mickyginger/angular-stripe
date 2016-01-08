c6d26a98023125b4e82a093e37ead6ba7fec66ae
panox <panospalos@gmail.com>
2016-01-08 16:45:20 +0000

    removed my keys reverted to orignal ones

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,6 +2,5 @@ angular
   .module('angularStripe', [])
   .constant('API_URL', 'http://localhost:3000')
   .config(function() {
-    // Stripe.setPublishableKey('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
-    Stripe.setPublishableKey('pk_test_hH5Fe3p7JU0r88Hklk3Y6J7m');
+    Stripe.setPublishableKey('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
   });
